aff617854af5055d6fc7721d2ddee6872e5816ce
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-09-14 00:42:41 +0200

    Some kind of 2048 in Haskell.

---
+++ 2048.hs
@@ -0,0 +1,82 @@
+import System.IO
+
+data Direction = East | North | West | South
+
+type Grid = [[Int]]
+
+start :: Grid
+start = [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 2],
+         [0, 0, 0, 2]]
+
+main :: IO ()
+main = do
+  -- pGrid start
+  -- putStrLn ""
+  -- pGrid $ move start West
+  -- putStrLn ""
+  -- pGrid $ transpose start
+  -- putStrLn ""
+  -- pGrid $ move (transpose start) West
+  game start
+
+game grid = do
+  putStrLn ""
+  pGrid grid
+  ch <- getCh
+--  direction <- charToDirection ch
+--  game $ move grid direction
+  game $ move grid (charToDirection ch) -- direction
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           c <- getChar
+           hSetEcho stdin True
+           return c
+
+charToDirection :: Char -> Maybe Direction
+charToDirection ch =
+  case ch of
+    'a' -> Just West
+    's' -> Just South
+    'd' -> Just East
+    'w' -> Just North
+    _   -> Nothing
+
+move :: Grid -> Maybe Direction -> Grid
+move g Nothing = g
+move g (Just North) = transpose $ map merge (transpose g)
+move g (Just East)  = colRev $ map merge (colRev g)
+move g (Just South) = transpose $ rowRev $ map merge (rowRev $ transpose g)
+move g (Just West)  = map merge g
+
+merge :: [Int] -> [Int]
+merge xs = merged ++ padding
+  where
+    merged = combine $ filter (/= 0) xs
+    padding = replicate (length xs - length merged) 0
+    combine (x:y:xs) | x == y = 2 * x : combine xs
+                     | otherwise =  x : combine (y:xs)
+    combine x        = x
+
+transpose :: Grid -> Grid
+transpose [[aa, ab, ac, ad],
+           [ba, bb, bc, bd],
+           [ca, cb, cc, cd],
+           [da, db, dc, dd]] =
+  [[aa, ba, ca, da],
+   [ab, bb, cb, db],
+   [ac, bc, cc, dc],
+   [ad, bd, cd, dd]]
+
+colRev :: Grid -> Grid
+colRev = map reverse
+
+rowRev :: Grid -> Grid
+rowRev = reverse
+
+pGrid (r:[]) = print r
+pGrid (r:rs) = do
+  print r
+  pGrid rs
