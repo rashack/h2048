715a07a873be6cae0ff90334565429a7a2382cb2
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-09-18 23:33:22 +0200

    Ignore impossible moves.

--- h2048.hs
+++ h2048.hs
@@ -10,18 +10,27 @@ main :: IO ()
 main = do
   r <- Rnd.getStdGen
   let (g, r') = GridOps.startGrid r
+  GridOps.pGrid g
   game g r'
   return ()
 
 game :: Grid -> StdGen -> IO Grid
 game grid rnd = do
-  putStrLn ""
-  GridOps.pGrid grid
   ch <- getCh
   case charToDirection ch of
-    Just char -> do let (g', rnd') = GridOps.spawn rnd $ GridOps.move grid char
-                    game g' rnd'
+    Just char -> do (grid', rnd') <- maybeMove grid char rnd
+                    game grid' rnd'
     Nothing   -> game grid rnd
+
+maybeMove :: Grid -> Direction -> StdGen -> IO (Grid, StdGen)
+maybeMove g c rnd = if g == g'
+                    then return (g, rnd)
+                    else do putStrLn ""
+                            GridOps.pGrid g''
+                            return (g'', rnd')
+  where g' = GridOps.move g c
+        (g'', rnd') = GridOps.spawn rnd g'
+
 
 getCh :: IO Char
 getCh = do hSetEcho stdin False
