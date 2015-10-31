5d728aae3c11971bc548cd6fdaaafe102bcd43a7
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-10-31 08:01:29 +0100

    Using HSCurses, but there is some kind of bug.

--- src/h2048.hs
+++ src/h2048.hs
@@ -2,26 +2,36 @@ module Main where
 
 import Control.Monad.State
 import Grid
-import System.IO
 import System.Random
-import Text.Printf
+import UI.HSCurses.Curses
 import qualified GridOps
 
 main :: IO ()
 main = do
+  initCurses
+  keypad stdScr True -- make the cursor keys usable
+  echo False
+  cursSet CursorInvisible
   r <- getStdGen
   let (g, r') = runState GridOps.startGrid r
   ppGrid g
   game g r'
-  return ()
+  endWin
 
 game :: Grid -> StdGen -> IO Grid
 game grid rnd = do
   ch <- getCh
-  case charToDirection ch of
-    Just char -> do (grid', rnd') <- maybeMove grid char rnd
-                    game grid' rnd'
-    Nothing   -> game grid rnd
+  case ch of
+    KeyLeft  -> tryMove grid West rnd
+    KeyDown  -> tryMove grid South rnd
+    KeyRight -> tryMove grid East rnd
+    KeyUp    -> tryMove grid North rnd
+    KeyChar 'q' -> return grid
+    _           -> game grid rnd
+
+tryMove :: Grid -> Direction -> StdGen -> IO Grid
+tryMove grid dir rnd = do (grid', rnd') <- maybeMove grid dir rnd
+                          game grid' rnd'
 
 maybeMove :: Grid -> Direction -> StdGen -> IO (Grid, StdGen)
 maybeMove g c rnd = if g == g'
@@ -32,41 +42,22 @@ maybeMove g c rnd = if g == g'
   where g' = GridOps.move g c
         (g'', rnd') = runState (GridOps.spawn g') rnd
 
-getCh :: IO Char
-getCh = do hSetEcho stdin False
-           buffering <- hGetBuffering stdin
-           hSetBuffering stdin NoBuffering
-           c <- getChar
-           hSetEcho stdin True
-           hSetBuffering stdin buffering
-           return c
-
-commands :: [(Char, Direction)]
-commands = [ ('a', West), ('s', South), ('d', East), ('w', North) ]
-
-charToDirection :: Char -> Maybe Direction
-charToDirection ch = lookup ch commands
-
-pGrid :: Grid -> IO ()
-pGrid [] = return ()
-pGrid (r:[]) = print r
-pGrid (r:rs) = do
-  print r
-  pGrid rs
-
 ppGrid :: Grid -> IO ()
-ppGrid g = ppGrid2 g width
-  where width = 1 + length (show $ maximum $ map maximum g)
+ppGrid g = do
+  erase
+  mvPutGrid 0 0 g
+  refresh
 
-ppGrid2 :: Grid -> Int -> IO ()
-ppGrid2 [] _ = return ()
-ppGrid2 (r:[]) width = ppRow r width
-ppGrid2 (r:rs) width = do
-  ppRow r width
-  ppGrid2 rs width
+mvPutRow :: Int -> Int -> Int -> [Int] -> IO ()
+mvPutRow y x w row = mvWAddStr stdScr y x $ concat $ map (xpnd w) row
 
-ppRow :: [Int] -> Int -> IO ()
-ppRow r w = do
-  mapM_ (Text.Printf.printf ("%" ++ show w ++ "d")) r
-  putStrLn ""
+mvPutGrid :: Int -> Int -> [[Int]] -> IO ()
+mvPutGrid y x g = do
+  mapM (\(y', r) -> mvPutRow y' x w r) $ zip [y..] g
   return ()
+    where w = 1+ (length $ show (maximum $ map maximum g))
+
+xpnd :: Int -> Int -> String
+xpnd w i = replicate (w - l) ' ' ++ s
+  where s = show i
+        l = length s
