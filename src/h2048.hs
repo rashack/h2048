27d5fb102be8352368ae16c21d4d6b1f93a426ac
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-02-27 21:26:49 +0100

    Using Brick/vty for ncurses rendering.

--- src/h2048.hs
+++ src/h2048.hs
@@ -1,72 +1,75 @@
 module Main where
 
+import Brick
+import qualified Brick.AttrMap as A
+import qualified Brick.Main as BM
+import qualified Brick.Types as BT
+import qualified Brick.Widgets.List as L
 import Control.Monad.State
-import Grid
-import System.IO
+import Grid as G
 import System.Random
-import Text.Printf
+import qualified Graphics.Vty as V
 import qualified GridOps
+
+ui :: (Grid, StdGen) -> [Widget]
+ui (g, _r) = [str $ grid2str g]
+
+appEvent :: (Grid, StdGen) -> V.Event -> BT.EventM (BT.Next (Grid, StdGen))
+appEvent (g, r) e =
+  case e of
+    V.EvKey V.KEsc []   -> BM.halt (g, r)
+    V.EvKey V.KRight [] -> BM.continue $ maybeMove g East r
+    V.EvKey V.KLeft []  -> BM.continue $ maybeMove g West r
+    V.EvKey V.KUp []    -> BM.continue $ maybeMove g North r
+    V.EvKey V.KDown []  -> BM.continue $ maybeMove g South r
+    _ev                 -> BM.continue (g, r)
+
+theMap :: A.AttrMap
+theMap = A.attrMap V.defAttr
+    [ (L.listAttr,            V.white `on` V.blue)
+    , (L.listSelectedAttr,    V.blue `on` V.white)
+    ]
+
+app :: BM.App (Grid, StdGen) V.Event
+app =
+  BM.App { BM.appDraw = ui
+         , BM.appChooseCursor = BM.showFirstCursor
+         , BM.appHandleEvent = appEvent
+         , BM.appStartEvent = return
+         , BM.appAttrMap = const theMap
+         , BM.appLiftVtyEvent = id
+         }
 
 main :: IO ()
 main = do
   r <- getStdGen
   let (g, r') = runState GridOps.startGrid r
-  ppGrid g
-  game g r'
-  return ()
+  void $ BM.defaultMain app (g, r')
 
-game :: Grid -> StdGen -> IO Grid
-game grid rnd = do
-  ch <- getCh
-  case charToDirection ch of
-    Just char -> do (grid', rnd') <- maybeMove grid char rnd
-                    game grid' rnd'
-    Nothing   -> game grid rnd
-
-maybeMove :: Grid -> Direction -> StdGen -> IO (Grid, StdGen)
+maybeMove :: Grid -> G.Direction -> StdGen -> (Grid, StdGen)
 maybeMove g c rnd = if g == g'
-                    then return (g, rnd)
-                    else do putStrLn ""
-                            ppGrid g''
-                            return (g'', rnd')
+                    then (g, rnd)
+                    else (g'', rnd')
   where g' = GridOps.move g c
         (g'', rnd') = runState (GridOps.spawn g') rnd
 
-getCh :: IO Char
-getCh = do hSetEcho stdin False
-           buffering <- hGetBuffering stdin
-           hSetBuffering stdin NoBuffering
-           c <- getChar
-           hSetEcho stdin True
-           hSetBuffering stdin buffering
-           return c
-
-commands :: [(Char, Direction)]
-commands = [ ('a', West), ('s', South), ('d', East), ('w', North) ]
-
-charToDirection :: Char -> Maybe Direction
-charToDirection ch = lookup ch commands
-
-pGrid :: Grid -> IO ()
-pGrid [] = return ()
-pGrid (r:[]) = print r
-pGrid (r:rs) = do
-  print r
-  pGrid rs
-
-ppGrid :: Grid -> IO ()
-ppGrid g = ppGrid2 g width
+grid2str :: Grid -> String
+grid2str g = grid2str2 width g
   where width = 1 + length (show $ maximum $ map maximum g)
 
-ppGrid2 :: Grid -> Int -> IO ()
-ppGrid2 [] _ = return ()
-ppGrid2 (r:[]) width = ppRow r width
-ppGrid2 (r:rs) width = do
-  ppRow r width
-  ppGrid2 rs width
+grid2str2 :: Int -> Grid -> String
+grid2str2 width grid = foldl (++) [] $ lineBreaks $ map (row2str width) grid
 
-ppRow :: [Int] -> Int -> IO ()
-ppRow r w = do
-  mapM_ (Text.Printf.printf ("%" ++ show w ++ "d")) r
-  putStrLn ""
-  return ()
+row2str :: Int -> [Int] -> String
+row2str width row =
+  foldl (++) [] $ map (padNum width) row
+
+padNum :: Int -> Int -> String
+padNum width num = padding ++ numStr
+  where padding = foldl (++) [] $ replicate (width - length numStr) " "
+        numStr = show num
+
+lineBreaks :: [String] -> [String]
+lineBreaks []       = []
+lineBreaks (x:y:[]) = [x] ++ ["\n"] ++ [y]
+lineBreaks (x:xs)   = [x] ++ ["\n"] ++ lineBreaks xs
